8c75a608abdd2387105a91679ba7c10c73bd5f1e
himanshu-chaddha <himanshu.chaddha@mail.vinove.com>
2020-06-20 19:03:15 +0530

    creating snippets

---
+++ const.js
@@ -0,0 +1,32 @@
+/* most essential keyword provided by ES6 to JS i.e const
+ declaring a variable using const */
+
+const name = "Himanshu";
+// printing that variable
+name;
+
+/* we can't change const variable having primitive datatypes as values
+changing name with throw an error */
+
+// name = "Harsh";
+
+/* Initializing is must while declaring the const variables
+    if we don't do that it again throws an error
+*/
+
+// const age;
+
+/* Declaring arrays and JS objects as empty using const
+    it will allow them to do so, because adding or removing
+    from these will not change the reference of the objects */
+
+const arr = [];
+arr[0] = 1;
+// printing arr
+arr;
+
+const obj = {};
+obj.name = "Himanshu";
+//printing obj
+obj;
+
---
+++ demo.js
@@ -0,0 +1,10 @@
+/* defining variables and printing them diectly using quokka.js extension
+ declaring a variable "a" */
+var a = "hello";
+// printing variable "a" simply writing variable name
+a;
+
+/* instead of printing single number variable we can print any object */
+
+a = { name: "Himanshu", age: 24 };
+a;
